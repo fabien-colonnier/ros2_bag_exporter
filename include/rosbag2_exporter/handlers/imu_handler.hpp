07edd0d0bfa7d76d03f6932bb5317f39a4b80726
fabien <fabien@opteran.com>
2025-01-20 16:46:17 +0000

    store the imu in one csv file

--- include/rosbag2_exporter/handlers/imu_handler.hpp
+++ include/rosbag2_exporter/handlers/imu_handler.hpp
@@ -22,7 +22,36 @@ public:
   // Constructor to accept logger
   IMUHandler(const std::string & output_dir, rclcpp::Logger logger)
   : BaseHandler(logger), output_dir_(output_dir)
-  {}
+  {
+    // Create the full file path with '.csv' as the extension
+    std::string filepath = output_dir_ + "/data.csv";
+
+    // Ensure the directory exists, create if necessary
+    std::filesystem::path dir_path = output_dir_;
+    if (!std::filesystem::exists(dir_path)) {
+      RCLCPP_INFO(logger_, "Creating directory: %s", dir_path.c_str());
+      std::filesystem::create_directories(dir_path);
+    }
+
+    // Open file and write IMU data as CSV
+    outfile_ = std::ofstream(filepath);
+    if (!outfile_.is_open()) {
+      RCLCPP_ERROR(logger_, "Failed to open file to write IMU data: %s", filepath.c_str());
+      return;
+    }
+
+    // Write first line
+    outfile_ << "timestamp [ns],"
+            << "angular_velocity_x [rad s^-1],angular_velocity_y [rad s^-1],angular_velocity_z [rad s^-1],"
+            << "linear_acceleration_x [m s^-2],linear_acceleration_y [m s^-2],linear_acceleration_z [m s^-2],"
+            << "orientation_x,orientation_y,orientation_z,orientation_w," << std::endl;
+  }
+
+  // Constructor to accept logger
+  ~IMUHandler()
+  {
+    outfile_.close();
+  }
 
   void process_message(const rclcpp::SerializedMessage & serialized_msg,
                       const std::string & topic,
@@ -35,50 +64,23 @@ public:
 
     // Create a timestamped filename
     std::stringstream ss_timestamp;
-    ss_timestamp << imu_data.header.stamp.sec << "-"
+    ss_timestamp << imu_data.header.stamp.sec
                 << std::setw(9) << std::setfill('0') << imu_data.header.stamp.nanosec;
     std::string timestamp = ss_timestamp.str();
 
-    // Sanitize the topic name by removing the leading '/'
-    std::string sanitized_topic = topic;
-    if (!sanitized_topic.empty() && sanitized_topic[0] == '/') {
-      sanitized_topic = sanitized_topic.substr(1);
-    }
-
-    // Create the full file path with '.csv' as the extension
-    std::string filepath = output_dir_ + "/" + sanitized_topic + "/" + timestamp + ".csv";
-
-    // Ensure the directory exists, create if necessary
-    std::filesystem::path dir_path = output_dir_ + "/" + sanitized_topic;
-    if (!std::filesystem::exists(dir_path)) {
-      RCLCPP_INFO(logger_, "Creating directory: %s", dir_path.c_str());
-      std::filesystem::create_directories(dir_path);
-    }
-
-    // Open file and write IMU data as CSV
-    std::ofstream outfile(filepath);
-    if (!outfile.is_open()) {
-      RCLCPP_ERROR(logger_, "Failed to open file to write IMU data: %s", filepath.c_str());
-      return;
-    }
-
-    // Write IMU data (orientation, angular velocity, linear acceleration)
-    outfile << "timestamp," << "orientation_x,orientation_y,orientation_z,orientation_w,"
-            << "angular_velocity_x,angular_velocity_y,angular_velocity_z,"
-            << "linear_acceleration_x,linear_acceleration_y,linear_acceleration_z" << std::endl;
-    outfile << timestamp << ","
-            << imu_data.orientation.x << "," << imu_data.orientation.y << "," << imu_data.orientation.z << "," << imu_data.orientation.w << ","
+    // Write IMU data (angular velocity, linear acceleration, orientation)
+    outfile_ << timestamp << ","
             << imu_data.angular_velocity.x << "," << imu_data.angular_velocity.y << "," << imu_data.angular_velocity.z << ","
-            << imu_data.linear_acceleration.x << "," << imu_data.linear_acceleration.y << "," << imu_data.linear_acceleration.z
+            << imu_data.linear_acceleration.x << "," << imu_data.linear_acceleration.y << "," << imu_data.linear_acceleration.z << ","
+            << imu_data.orientation.x << "," << imu_data.orientation.y << "," << imu_data.orientation.z << "," << imu_data.orientation.w
             << std::endl;
 
-    outfile.close();
-
-    RCLCPP_INFO(logger_, "Successfully wrote IMU data to %s", filepath.c_str());
+    RCLCPP_INFO(logger_, "Successfully wrote IMU data, timestamp = %s ns", timestamp.c_str());
   }
 
 private:
   std::string output_dir_;
+  std::ofstream outfile_;
 };
 
 }  // namespace rosbag2_exporter
